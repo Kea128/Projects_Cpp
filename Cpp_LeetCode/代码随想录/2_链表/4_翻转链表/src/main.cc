6b0a9d6c292719e9eeb95298956cf608ae23b6cf
Kea128 <1287811578@qq.com>
2024-09-03 10:16:55 +0800

    ver_240903_1016

---
+++ Cpp_LeetCode/代码随想录/2_链表/4_翻转链表/src/main.cc
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+/**
+ * @brief 206. 反转链表
+ */
+
+/*
+  给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+
+  示例1：
+  输入：head = [1,2,3,4,5]
+  输出：[5,4,3,2,1]
+
+  示例2：
+  输入：head = [1,2]
+  输出：[2,1]
+
+  示例3：
+  输入：head = []
+  输出：[]
+*/
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+};
+
+int main() {
+  int n;
+  while (true) {
+    std::cin >> n;
+  }
+}
