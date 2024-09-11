4bb031d0135d5fd05e2310ba45f9127f4b46feee
Kea128 <1287811578@qq.com>
2024-09-12 00:43:06 +0800

    ver_240912_0043

--- Cpp_LeetCode/代码随想录/8_回溯算法/5_电话号码的字母组合/src/main.cc
+++ Cpp_LeetCode/代码随想录/8_回溯算法/5_电话号码的字母组合/src/main.cc
@@ -13,14 +13,50 @@
 
 std::string path;
 std::vector<std::string> res;
+std::map<char, std::string> mp;
 
-void backtracking(std::string& str, int index) {}
+void backtracking(std::string& str, int index) {
+  if (str.empty()) return;
+  if (path.size() == str.size()) {
+    res.emplace_back(path);
+    return;
+  }
+  for (int i = index; i < str.size(); i++) {
+    char jian = str[i];
+    std::string words = mp[jian];
+    for (int j = 0; j < words.size(); j++) {
+      path.push_back(words[j]);
+      backtracking(str, i + 1);
+      path.pop_back();
+    }
+  }
+}
 
-std::vector<std::string> letterCombinations(std::string digits) {}
+std::vector<std::string> letterCombinations(std::string digits) {
+  backtracking(digits, 0);
+  return res;
+}
 
 int main(int argc, char* argv[]) {
-  std::map<int, std::string> mp;
-  mp[2] = "abc";
+  mp['2'] = "abc";
+  mp['3'] = "def";
+  mp['4'] = "ghi";
+  mp['5'] = "jkl";
+  mp['6'] = "mno";
+  mp['7'] = "pqrs";
+  mp['8'] = "tuv";
+  mp['9'] = "wxyz";
+
+  std::string str;
+  std::cin >> str;
+  auto res = letterCombinations(str);
+
+  for (auto& i : res) {
+    for (auto& j : i) {
+      std::cout << j << " ";
+    }
+    std::cout << std::endl;
+  }
 
   return 0;
 }
---
+++ Cpp_LeetCode/代码随想录/9_贪心算法/2_分发饼干/src/main.cc
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+
+/**
+ * @brief 455分发饼干
+ * https://leetcode.cn/problems/assign-cookies/
+ */
