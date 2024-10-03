7b6197935ac87904bbef46e6bf6529163f545a52
Kea128 <1287811578@qq.com>
2024-10-03 23:24:52 +0800

    ver_241003_2324

--- Cpp_LeetCode/代码随想录/10_动态规划/213_打家劫舍II/src/main.cc
+++ Cpp_LeetCode/代码随想录/10_动态规划/213_打家劫舍II/src/main.cc
@@ -8,13 +8,27 @@ using namespace std;
  * https://leetcode.cn/problems/house-robber-ii/description/
  */
 
-int rob(vector<int>& nums) {
-  std::vector<int> nums2(nums.rbegin(), nums.rend());
-  std::vector<int> dp_left(nums.size(), 0);
-  std::vector<int> dp_right(nums.size(), 0);
-  dp_left[0] = nums[0];
+int cal(vector<int>& nums, int start, int end) {
+  if (start > end) return 0;
+  if (start == end) return nums[start];
+  vector<int> dp(nums.size(), 0);
+  dp[start] = nums[start];
+  dp[start + 1] = max(nums[start], nums[start + 1]);
+  for (int i = start + 2; i <= end; i++) {
+    dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+  }
+  return dp[end];
+}
 
-  return 1;
+int rob(vector<int>& nums) {
+  if (nums.size() == 0) return 0;
+  if (nums.size() == 1) return nums[0];
+
+  // 取nums[0]
+  int value1 = nums[0] + cal(nums, 2, nums.size() - 2);
+  // 不取nums[0]
+  int value2 = cal(nums, 1, nums.size() - 1);
+  return max(value1, value2);
 }
 
 int main(int argc, char const* argv[]) {
