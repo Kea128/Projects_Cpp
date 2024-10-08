695452dadec238c8c917d897f133591436a0866b
Kea128 <1287811578@qq.com>
2024-10-09 01:02:39 +0800

    ver_241009_0102

---
+++ Cpp_LeetCode/随机一题/2187_完成旅途的最少时间/src/main.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+/**
+ * @brief 2187_完成旅途的最少时间
+ * https://leetcode.cn/problems/minimum-time-to-complete-trips/description/
+ */
+
+long long minimumTime(vector<int>& time, int totalTrips) {}
+
+int main(int argc, char const* argv[]) {}
