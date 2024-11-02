6e2fce8bcac6965092dfdd63ce28e43877a8357a
Kea128 <1287811578@qq.com>
2024-11-02 18:58:19 +0800

    ver_241102_1858

---
+++ 笔试/美团/1/src/main_1.cc
@@ -0,0 +1,25 @@
+// #include <algorithm>
+// #include <iostream>
+// #include <map>
+// #include <set>
+// #include <sstream>
+// #include <vector>
+// using namespace std;
+
+// int solution(int n, int a, int b, int c) {
+//   int res = 0;
+//   int cap = std::max(std::max(a, b), c);
+//   while (n > 0) {
+//     res++;
+//     n -= cap;
+//   }
+//   return res;
+// }
+
+// int main(int argc, char const* argv[]) {
+//   int n;
+//   int a, b, c;
+//   std::cin >> n >> a >> b >> c;
+//   std::cout << solution(n, a, b, c) << std::endl;
+//   return 0;
+// }
